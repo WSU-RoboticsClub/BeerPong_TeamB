9238cbb5242b072fa38ba0856c2aa5f8e886b960
Brandon Kallaher <brandonj@kallaher.org>
2015-12-03 17:59:37 -0800

    Motors will now run based on timer 1.

--- Microcontroller/src/main.c
+++ Microcontroller/src/main.c
@@ -18,10 +18,13 @@
  Processor Configuration
  ************************************************************************/
 
-
-
 //forward declarations
-void timer_callback(void);
+void timer1_callback(void);
+
+//Global Vars
+uint8 speed = 1;
+uint8 direction = 0;
+PWM_Config pwm1_config = {0}, pwm2_config = {0};
 
 /*************************************************************************
  Main Function
@@ -29,16 +32,59 @@ void timer_callback(void);
 int main(void) {
 
     //structures for configuring peripherals
-    Timer_Config timer_config = {0};
-
-    //setup peripherals
-    timer_config.frequency = 1; //Have the timer trigger at a rate of 1Hz
-    timer_config.pbclk = 15000000; //The peripheral bus clock is configured to operate at 15MHz
-    timer_config.which_timer = Timer_1; //Use Timer 1
-    timer_config.callback = &timer_callback; //Hand a callback function for the ISR
-    timer_config.enabled = 1; //Enable the Timer
-    initialize_Timer(timer_config); //Initialize the timer module
+    Timer_Config timer1_config = {0}, timer2_config = {0}, timer3_config = {0};
+    //setup Timer 1
+    timer1_config.frequency = 1; //Have the timer trigger at a rate of 1Hz
+    timer1_config.pbclk = 15000000; //The peripheral bus clock is configured to operate at 15MHz
+    timer1_config.which_timer = Timer_1; //Use Timer 1
+    timer1_config.callback = &timer1_callback; //Hand a callback function for the ISR
+    timer1_config.enabled = 1; //Enable the Timer
+    initialize_Timer(timer1_config); //Initialize the timer module
  
+    //Setup Timer 2
+    timer2_config.frequency = 5000; //Change later
+    timer2_config.pbclk = 15000000;
+    timer2_config.which_timer = Timer_2;
+    timer2_config.callback = NULL;
+    timer2_config.enabled = 1;
+    initialize_Timer(timer2_config);
+    
+    //Setup Timer 3
+    timer3_config.frequency = 5000; //Change later
+    timer3_config.pbclk = 15000000;
+    timer3_config.which_timer = Timer_3;
+    timer3_config.callback = NULL;
+    timer3_config.enabled = 1;
+    initialize_Timer(timer3_config);
+    
+    //Setup PWM 1
+    pwm1_config.timer = timer2_config; //Use Timer 2
+    pwm1_config.channel = PWM_CH_1;
+    pwm1_config.pin = Pin_RPB3; //Use Pin 7
+    pwm1_config.dutyCycle = 0; //Initialize with 0% duty cycle
+    pwm1_config.enable = 0; //Initially Disable the output
+    initialize_PWM(pwm1_config);
+    
+    //Setup PWM 2
+    pwm2_config.timer = timer3_config; //Use Timer 3
+    pwm2_config.channel = PWM_CH_2;
+    pwm2_config.pin = Pin_RPB5; //Use Pin 14
+    pwm2_config.dutyCycle = 0; //Initialize with 0% duty cycle
+    pwm2_config.enable = 0; //Initially Disable the output
+    initialize_PWM(pwm2_config);
+    
+    //Setup the direction pin outputs 
+    //M1
+    //Use RB1 (pin 5) and RB2 (pin 6) & Disable AN3 and AN4
+    TRISBbits.TRISB1 = 0; //Enable RB1 as output
+    ANSELBbits.ANSB1 = 0; //Disable AN3
+    TRISBbits.TRISB2 = 0; //Enable RB2 as output
+    ANSELBbits.ANSB2 = 0; //Disable AN4
+    //M2
+    //Use RB7 (pin 16) and RB8 (pin 17)
+    TRISBbits.TRISB7 = 0; //Enable RB7 as output
+    TRISBbits.TRISB8 = 0; //Enable RB8 as output
+    
     //Global interrupt enable. Do this last!
     INTEnableSystemMultiVectoredInt();
     asm volatile ("ei"); //reenable interrupts
@@ -50,7 +96,44 @@ int main(void) {
     return 0;
 }
 
-void timer_callback(void)
+void timer1_callback(void)
 {
-    
+    //Every Second change the speed and/or direction
+    switch (speed)
+    {
+        case 1:
+            update_PWM(pwm1_config, 0.25);
+            speed++;
+            break;
+        case 2:
+            update_PWM(pwm1_config, 0.5);
+            speed++;
+            break;
+        case 3:
+            update_PWM(pwm1_config, 0.75);
+            speed++;
+            break;
+        default:
+            //Switch Direction
+            if (direction == 0)
+            {
+                disable_PWM(pwm1_config);
+                LATBbits.LATB1 = 1;
+                LATBbits.LATB2 = 0;
+                update_PWM(pwm1_config, 0);
+                enable_PWM(pwm1_config);
+                direction = 1;
+            }
+            else
+            {
+                disable_PWM(pwm1_config);
+                LATBbits.LATB1 = 0;
+                LATBbits.LATB2 = 1;
+                update_PWM(pwm1_config, 0);
+                enable_PWM(pwm1_config);
+                direction = 0;
+            }
+            speed = 1;
+            break;
+    }
 }
